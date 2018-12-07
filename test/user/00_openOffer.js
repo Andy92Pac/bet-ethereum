ca8231fa7e5d3e9e06447b8981e3907db1f62103
andy92pac <andy.mpondo@gmail.com>
2018-12-07 21:49:43 +0100

    update tests

--- test/user/00_openOffer.js
+++ test/user/00_openOffer.js
@@ -75,7 +75,7 @@ contract('SocialBet', (accounts) => {
 
 		assert.equal(parseInt(nbOffers), parseInt(oldNbOffers));
 	});
-
+*/
 	it("should open offer", async () => {
 
 		var snapshotId = (await utils.snapshot()).result;
@@ -108,7 +108,7 @@ contract('SocialBet', (accounts) => {
 
 		await utils.revert(snapshotId);
 	});
-*/
+
 	it("should abort because event is not available", async () => {
 
 		var snapshotId = (await utils.snapshot()).result;
